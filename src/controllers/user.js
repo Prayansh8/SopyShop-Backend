8620dbd5baa8d9c99b7e1f6a80921e55064f6155
Prayansh <prayansh811@gmail.com>
2023-01-30 18:32:25 +0530

    add the authentication

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const { config } = require("../config");
 const bcrypt = require("bcrypt");
 
-const signUp = async (req, res) => {
+const signUp = async (req, res, next) => {
   const { name, username, email, password } = req.body;
 
   if (name === "") {
@@ -59,35 +59,49 @@ const signIn = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  var ere = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var ere =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (!ere.test(email)) {
-      return res.status(400).send({ detail: 'Please fill email formate' });
+    return res.status(400).send({ detail: "Please fill email formate" });
   }
-  
-  var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,32}$/;
+
+  var re =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,32}$/;
   if (!re.test(password)) {
-    return res.status(400).send({ detail: 'Minimum 6 and maximum 32 characters, at least one uppercase letter, one lowercase letter, one number and one special character:' });
+    return res.status(400).send({
+      detail:
+        "Minimum 6 and maximum 32 characters, at least one uppercase letter, one lowercase letter, one number and one special character:",
+    });
   }
-  
+
   const user = await db.user.findOne({ email: email });
   if (!user) {
-    return res.status(404).send({ detail: 'User not found' });
+    return res.status(404).send({ detail: "User not found" });
   }
   const validPassword = await bcrypt.compare(password, user.password);
   if (!validPassword) {
-    return res.status(400).send({ detail: 'Invalid credentials' });
+    return res.status(400).send({ detail: "Invalid credentials" });
   }
-  
+
   const userData = {
-    id: user._id,
-    name: user.name,
-    username: user.username,
-    email: user.email,
-  }
+    // id: user._id,
+    // name: user.name,
+    // username: user.username,
+    // email: user.email,
+    user,
+  };
   const token = jwt.sign(userData, config.jwt.jwtSecretKey);
-  return res.send({ detail: 'Login success', token: token, userData: userData });
-  
+
+  const option = {
+    expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  }; 
+  return res
+    .status(200)
+    .cookie("token", token, option)
+    .send({ detail: "Login success", token: token, userData: userData });
 };
+
 
 module.exports = {
   signUp,
