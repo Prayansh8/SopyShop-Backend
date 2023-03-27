fc189949aa9553a307699362fd7bcf1cef6fb3fc
Prayansh <prayansh811@gmail.com>
2023-03-27 20:01:39 +0530

    update the reset password token
    add a function resetPassword and use put method in reset password token

--- src/controllers/userAuth.js
+++ src/controllers/userAuth.js
@@ -1,7 +1,7 @@
 const { db } = require("../databases/index");
 const catchAsyncErrors = require("../middlewere/catchAsyncErrors");
 const { sendMailler } = require("../utills/sendMailler");
-
+const crypto = require("crypto");
 const getUsers = async (req, res) => {
   const data = await db.user.find({}, { name: 1, userName: 1, email: 1 });
   return res.send(data);
@@ -58,13 +58,12 @@ const forwordPassword = catchAsyncErrors(async (req, res, next) => {
 
   const resetToken = findUser.getResetPasswordToken();
 
-    await findUser.save({validateBeforeSave: false })
+  await findUser.save({ validateBeforeSave: false });
 
-    //Create reset password url
-    const resetUrl =`http://localhost:5000/api/v1/reset/password/${resetToken}`;
-    
-    const message = `Your password reset token is as follows:\n\n${resetUrl}\n\n If you have not requested this email, then please ignore.`
+  //Create reset password url
+  const resetUrl = `http://localhost:5000/api/v1/reset/password/${resetToken}`;
 
+  const message = `Your password reset token is as follows:\n\n${resetUrl}\n\n If you have not requested this email, then please ignore.`;
 
   try {
     await sendMailler({
@@ -74,15 +73,48 @@ const forwordPassword = catchAsyncErrors(async (req, res, next) => {
     });
     return res.status(200).send({
       success: true,
-      massage: `Email sent to ${findUser.email}} successfully`,
+      massage: `Email sent to ${findUser.email}} successfully ${resetUrl}`,
     });
   } catch (error) {
     findUser.resetPasswordToken = undefined;
     findUser.resetPasswordExplre = undefined;
 
     await findUser.save({ validateBeforeSave: false });
-    return next(res.status(500).send("email not send " + error.massage + error));
+    return next(
+      res.status(500).send("email not send " + error.massage + error)
+    );
   }
+});
+
+const resetPassword = catchAsyncErrors(async (req, res, next) => {
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await db.user.findOne({
+    resetPasswordToken,
+    resetpasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      res
+        .status(404)
+        .send("reset password token is invalid or has been expired" + user)
+    );
+  }
+
+  if (req.body.password !== req.body.comfirmPassword) {
+    return next(res.status(404).send("Password dose not match"));
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExplre = undefined;
+
+  await user.save();
+  return next(res.status(200).send(res + user));
 });
 
 module.exports = {
@@ -92,4 +124,5 @@ module.exports = {
   deleteUser,
   logoutUser,
   forwordPassword,
+  resetPassword,
 };
