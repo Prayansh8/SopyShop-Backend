53cc25670b1f8536c1b8394f151e0b81da44fff0
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2024-05-24 23:38:41 +0530

    pusg

--- src/gts/gtsRoutes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authController = require("../gtsControllers/authController");
-const router = express.Router();
-router.post("/create-user", authController.register);
-router.post("/login", authController.login);
-router.get("/users", authController.getAllUsers);
-router.get("/:id", authController.getUser);
-router.patch("/update/:id", authController.updateUser);
-module.exports = router;
